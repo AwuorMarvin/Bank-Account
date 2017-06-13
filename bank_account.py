bb40e9d2b768ad45339d72068a3249fc15c16bf1
Awuor Marvin <awuor.marvin5@gmail.com>
2017-06-13 16:34:26 -0700

    Added the functionality to pass the tests in a ddition to adding various tests

--- bank_account.py
+++ bank_account.py
@@ -1,3 +1,18 @@
 class BankAccount(object):
-    def __init__(self):
-        pass
+    def __init__(self, account_balance = 500):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.amount = amount
+        self.account_balance += amount
+        return self.account_balance
+
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+        else:
+            return 'Your account balance is insufficient'
+
+class LeastBalance(BankAccount):
+    def __init__(self, least_amount = 200):
+        self.least_amount = least_amount
--- test_bank_account.py
+++ test_bank_account.py
@@ -1,5 +1,5 @@
 import unittest
-from bank_account import BankAccount
+from bank_account import BankAccount, LeastBalance
 
 class BankAccountClassTest(unittest.TestCase):
     """docstring for BankAccountClassTest"""
@@ -7,3 +7,25 @@ class BankAccountClassTest(unittest.TestCase):
     def test_subclass_instance(self):
         account1 = BankAccount()
         self.assertIsInstance(account1, BankAccount, msg='The object should be an instance of the `BankAccount` class')
+
+    def test_balance(self):
+        account1 = BankAccount()
+        self.assertEqual(account1.account_balance, 500)
+
+    def test_deposit(self):
+        account1 = BankAccount()
+        account1.deposit(3400)
+        self.assertEqual(account1.account_balance, 3900)
+
+    def test_withdraw(self):
+        account1 = BankAccount()
+        account1.withdraw(100)
+        self.assertEqual(account1.account_balance, 400)
+
+    def test_invalid_transaction(self):
+        account1 = BankAccount()
+        self.assertEqual(account1.withdraw(1000), "Your account balance is insufficient")
+
+    def test_sub_class(self):
+        account1 = BankAccount()
+        self.assertTrue(issubclass(LeastBalance, BankAccount))
